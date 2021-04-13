0d7a48374207cc9d4284b974a973280a1d7dce8a
Christian Back <cdirt27@gmail.com>
2021-04-13 02:47:10 -0500

    built for loop within .ready callback function which loops through each object in array people and displays an img of each object per githubUsername on the DOM

--- client.js
+++ client.js
@@ -1,1 +1,11 @@
 console.log('Here are all the available people:', people);
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+$(document).ready(function () {
+    for (let i= 0; i < people.length; i++) {
+        $('body').append(`<div><img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of ${people[i].name}"></div>`);
+    }
+  });
